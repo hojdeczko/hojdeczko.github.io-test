fba6555d558c208f98f1e149a4b772b51218e91b
Anka Hojdeczko <ankah90@gmail.com>
2018-02-25 19:24:59 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var serve = require('gulp-serve');
+
+gulp.task('serve', serve('public'));
+gulp.task('serve-build', serve(['public', 'build']));
+gulp.task('serve-prod', serve({
+  root: ['public', 'build'],
+  port: 80
+}));
